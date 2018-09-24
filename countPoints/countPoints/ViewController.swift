02df61b7f1fff7fb0e2c3a3868c3c3b3f7740fb2
akkkkira <32874465+akkkkira@users.noreply.github.com>
2018-09-24 15:35:33 +0900

    update use UserDefaults

--- countPoints/countPoints/ViewController.swift
+++ countPoints/countPoints/ViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let defaults = UserDefaults.standard
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var minusGetter: UITextField!
+    
+    @IBOutlet weak var showPoints: UILabel!
+    
+    var num:Int = 0
+    var numOrigin:Int = 0
+    var minusPoints:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let textField = UITextField()
+        
+        textField.delegate = self
+        textField.placeholder = "points"
+        textField.resignFirstResponder()
+        
+        numOrigin = defaults.integer(forKey: "Points")
+        showPoints.text = String(numOrigin)
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func updateCounter(num : Int){
+        defaults.set(numOrigin + num, forKey: "Points")
 
-
+        showPoints.text = String(defaults.integer(forKey: "Points"))
+    }
+    
+    @IBAction func plus1Point(_ sender: Any) {
+        num += 1
+        updateCounter(num: num)
+    }
+    
+    @IBAction func plus3Points(_ sender: Any) {
+        num += 3
+        updateCounter(num: num)
+    }
+    
+    @IBAction func plus5Points(_ sender: Any) {
+        num += 5
+        updateCounter(num: num)
+    }
+    
+   
+    @IBAction func minusPoints(_ sender: Any) {
+        minusPoints = minusGetter.text!
+        num -= Int(minusPoints)!
+        updateCounter(num: num)
+    }
 }
 

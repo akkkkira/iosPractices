b1b8f9bfd23125010915d408b97bfbb430979de5
akkkkira <32874465+akkkkira@users.noreply.github.com>
2018-09-24 09:39:31 +0900

    add new file IPhoneConnectionTest

---
+++ countPoints/countPoints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  countPoints
+//
+//  Created by 笠井瑛 on 2018/09/24.
+//  Copyright © 2018年 笠井瑛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
